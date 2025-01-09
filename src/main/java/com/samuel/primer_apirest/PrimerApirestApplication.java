26aae7fa794cd265d7b896f776acba3f859259db
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-08 23:52:16 -0500

    Primer commit

---
+++ src/main/java/com/samuel/primer_apirest/PrimerApirestApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.primer_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuel/primer_apirest/PrimerApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.primer_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
