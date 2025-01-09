37cf3d1a084aa9187448d47de2479e37c8dc4fe4
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-09 00:04:40 -0500

    Paquete y entidad person creada

---
+++ src/main/java/com/samuel/primer_apirest/Person/Person.java
@@ -0,0 +1,23 @@
+package com.samuel.primer_apirest.Person;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //genera getters y setter, ademas de toString, hashCode y equals 
+@AllArgsConstructor //crea constructor con todos los parametros
+@NoArgsConstructor //crea constructor sin parametros
+@Entity
+public class Person {
+    @Id //define que el id es un id
+    @GeneratedValue //como se genera el valor
+    private Integer id;
+    @Basic
+    private String firstname;
+    private String lastname;
+    private String email;
+}
