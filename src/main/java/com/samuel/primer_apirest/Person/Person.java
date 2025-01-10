4758a4ff9f1a405216aa89ae41fc2ac986e4ddd3
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-10 14:24:33 -0500

    Se agrego un ejemplo de como puede funcionar @Scope en una clase

--- src/main/java/com/samuel/primer_apirest/Person/Person.java
+++ src/main/java/com/samuel/primer_apirest/Person/Person.java
@@ -1,5 +1,7 @@
 package com.samuel.primer_apirest.Person;
 
+import org.springframework.context.annotation.Scope;
+
 import jakarta.persistence.Basic;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor //crea constructor con todos los parametros
 @NoArgsConstructor //crea constructor sin parametros
 @Entity//crea en la BD
+//@Scope("singleton")
 public class Person {
     @Id //define que el id es un id
     @GeneratedValue //como se genera el valor
